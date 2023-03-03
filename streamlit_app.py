09ffb65bd1f13f330b689cd2e3438c5b0c48be59
Wassim Jabi <jabiw@cardiff.ac.uk>
2023-03-03 20:56:47 +0000

    Updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,13 @@
 import streamlit as st
 from topologicpy.Cell import Cell
+from topologicpy.Explode import Explode
 from topologicpy.Plotly import Plotly
 
 st.title('🎈 TopologicPy is on Streamlit!')
 
 torus = Cell.Torus()
-data = Plotly.DataByTopology(torus)
+cluster = Topology.Explode(torus)
+data = Plotly.DataByTopology(cluster)
 fig = Plotly.FigureByData(data)
 st.plotly_chart(fig)
 
